8fa2dd9c5966cefa8d4ad5d68dc79f97da6452ac
Uzeyir AYDIN <uzeyir.aydin@hotmail.com.tr>
2022-01-07 13:59:10 +0300

    Proje dosyası ekle.

---
+++ Caller expression attribute/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://twitter.com/okyrylchuk/status/1476926159078182912
+
+using System.Runtime.CompilerServices;
+
+void TestMethod(object obj, [CallerArgumentExpression("obj")] string? message = null)
+{
+    Console.WriteLine("Expression is " + message);
+}
+
+TestMethod(new object());
+TestMethod("hello");
+TestMethod(1 + 2 + 3);
+TestMethod(() => { });
+
+// Output: 
+// Expression is new object()
+// Expression is "hello"
+// Expression is 1 + 2 + 3
+// Expression is () => { }
+
